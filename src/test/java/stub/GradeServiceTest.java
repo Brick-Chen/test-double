713efc8088010655f4e8aa6a291aa87e4db89d2a
YumingChen <mathcym@outlook.com>
2020-09-15 18:12:17 +0800

    [yuming chen]: add stub test for GradeService using Mockito.

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -2,11 +2,18 @@ package stub;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
@@ -16,6 +23,16 @@ public class GradeServiceTest {
             return Arrays.asList(80.0, 90.0, 100.0);
         }
     }
+
+    @Mock
+    GradeSystem mockGradeSystem;
+
+    GradeService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new GradeService(mockGradeSystem);
+    }
     
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
@@ -24,4 +41,16 @@ public class GradeServiceTest {
         double result = gradeService.calculateAverageGrades(1);
         Assertions.assertEquals(90.0, result);
     }
+
+    @Test
+    public void should_return_90_using_stub_test_with_mockito() {
+        List<Double> points = new ArrayList<>();
+        points.add(80.0);
+        points.add(90.0);
+        points.add(100.0);
+        Mockito.when(mockGradeSystem.gradesFor(1)).thenReturn(points);
+        double score = service.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, score);
+    }
+
 }
