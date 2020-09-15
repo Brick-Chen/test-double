8d0d809596f23b1c43b29970e15ce835e878194d
YumingChen <mathcym@outlook.com>
2020-09-15 15:27:19 +0800

    [yuming chen] confirm the format of mock test with Mockito.

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -13,21 +13,19 @@ public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-    @Mock private DoorPanel doorPanel;
+    @Mock
+    private DoorPanel doorPanel;
+
     private SecurityCenter securityCenter;
 
 
     @BeforeEach
     public void setUp() {
-        doorPanel = Mockito.mock(DoorPanel.class);
         securityCenter = new SecurityCenter(doorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
-//        MockDoorPanel mockDoorPanel = new MockDoorPanel();
-//        SecurityCenter securityCenter = new SecurityCenter(mockDoorPanel);
-//        securityCenter.switchOn();
         securityCenter.switchOn();
         Mockito.verify(doorPanel).close();
     }
