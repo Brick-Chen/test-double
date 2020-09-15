8f88f6aebe9ab9b86a4bf3408fba6dcd0ded2c1b
YumingChen <mathcym@outlook.com>
2020-09-15 15:21:36 +0800

    [yuming chen] implement mock test with Mockito.

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -13,8 +13,7 @@ public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-    @Mock
-    private DoorPanel doorPanel;
+    @Mock private DoorPanel doorPanel;
     private SecurityCenter securityCenter;
 
 
