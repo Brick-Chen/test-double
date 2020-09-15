f7dfe3b17e1a36d4024fcd23dcbb13b3969ccbff
YumingChen <mathcym@outlook.com>
2020-09-15 15:20:02 +0800

    [yuming chen] implement mock test with Mockito.

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,17 +3,33 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+    @Mock
+    private DoorPanel doorPanel;
+    private SecurityCenter securityCenter;
+
 
     @BeforeEach
     public void setUp() {
+        doorPanel = Mockito.mock(DoorPanel.class);
+        securityCenter = new SecurityCenter(doorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+//        MockDoorPanel mockDoorPanel = new MockDoorPanel();
+//        SecurityCenter securityCenter = new SecurityCenter(mockDoorPanel);
+//        securityCenter.switchOn();
+        securityCenter.switchOn();
+        Mockito.verify(doorPanel).close();
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,27 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-
+    private static class StubGradeSystem extends GradeSystem {
+        public List<Double> gradesFor(long studentId) {
+            return Arrays.asList(80.0, 90.0, 100.0);
+        }
+    }
+    
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        GradeSystem gradeSystem = new StubGradeSystem();
+        GradeService gradeService = new GradeService(gradeSystem);
+        double result = gradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
     }
 }
